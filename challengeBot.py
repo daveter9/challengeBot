1e5846d686b2b595c1394d1552b980dea6d8a0b1
daveter9 <david@modare.nl>
2016-08-28 18:05:33 +0200

    Updated readme with todolist and explanation Added comments to python files

--- challengeBot.py
+++ challengeBot.py
@@ -1,22 +1,38 @@
-import discord
-from discord.ext import commands
+import discord #import the discord.py lib
+from discord.ext import commands #import the commands extension
+import asyncio #import asyncio for the sleep function
 
 description ='''
 challengeBot is a discord bot that provides weekly coding challenges
 from /r/dailyprogrammer. Bot created by the fellows of /r/programming
-'''
+''' #description of the bot
 
-startup_extensions = ['testModule']
+#list of modules that the bot imports
+startup_extensions = ['testModule'] 
 
+#init the bot class, the prefix for the commands, the bot description and help
 bot = commands.Bot(command_prefix='!', description=description, pm_help=True)
 
+
+#Makes a background task
+async def background_task():
+    #starts when the bot is done loading
+    await bot.wait_until_ready()
+    #while is True while the bot is active
+    while not bot.is_closed:
+        #Bot prints 'I am alive' every 5 seconds to your console
+        print('I am alive')
+        await asyncio.sleep(5)
+        
 @bot.event
+#triggers when the bot is done loading
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
+    #loads all the extions mentioned in the list above
     for extension in startup_extensions:
         try:
             bot.load_extension(extension)
@@ -25,7 +41,12 @@ async def on_ready():
             print('Failed to load extension {}\n{}'.format(extension, exc))
 
 @bot.command(description='Returns pong')
+#triggers when you type prefix+function name, so: !ping
 async def ping():
+    #await is a asyncio keyword and bot.say types something out in discord
     await bot.say('pong')
-    
-bot.run('')
+
+#starts the background tasks in another loop
+bot.loop.create_task(background_task())
+#starts the bot, you need to put in your bot token here obtained by discord    
+bot.run('MTk5MTU0MDQxNjUyMTE3NTA1.Clq8QA.0P4p9zkKVdUsEF1fnQ-ISY0YWuQ')
--- testModule.py
+++ testModule.py
@@ -2,13 +2,19 @@ import discord
 from discord.ext import commands
 
 class testModule():
+    #get bot class, you need this to do things such as bot.say
     def __init__(self, bot):
         self.bot = bot
 
+    #Pass_contenxt True to get the message the user send
     @commands.command(pass_context=True, description='Returns Hi + username')
+    #You can acces the message object throufh ctx
     async def hello(self, ctx):
+        #here I actually use the ctx object to get the author's name
         await self.bot.say('Hi {}!'.format(ctx.message.author.name))
 
+#every module requires a setup function, you need to pass bot
 def setup(bot):
+    #You add the cog to the main program, use bot.add_cog(modulename(bot))
     bot.add_cog(testModule(bot))
     
