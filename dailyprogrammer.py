87d2ad5b38ca5be166d0165ae0e9c857badf790c
Arnas <invisibleghostt@gmail.com>
2016-08-29 16:23:12 +0300

    Reworked function to work with all difficultys.

--- dailyprogrammer.py
+++ dailyprogrammer.py
@@ -7,27 +7,32 @@ class Scraper:
     user_agent = 'daily programmer test scrapper'
     thing_limit=10
 
-    already_done_easy=[]
+    already_done=[]
 
     def __init__(self):
         self.r= praw.Reddit(self.user_agent)
 
 
-    def get_newest_easy(self):
+    def get_newest(self,difficulty='Easy'):
+        '''
+        Gets newest submmisions from /r/ dailyprogrammer
+        :param difficulty: Easy, Intermediate, Hard
+        :return:  
+        '''
         subreddit = self.r.get_subreddit('dailyprogrammer')
         for submission in subreddit.get_hot(limit=10):
-            if submission.id not in self.already_done_easy and self._check_easy(submission.title):
+            if submission.id not in self.already_done and self._check(difficulty,submission.title):
                 print(submission.title)
-                self.already_done_easy.append(submission.id)
+                self.already_done.append(submission.id)
 
 
-    def _check_easy(self,title):
-        regex_easy = '.*?\[.*?(\[)(Easy).*?..*?..*?..*?..*?(.)'
+    def _check(self,difficulty,title):
+        regex_easy = '.*?\[.*?(\[)('+difficulty+').*?..*?..*?..*?..*?(.)'
         rg = re.compile(regex_easy,re.IGNORECASE | re.DOTALL)
         return rg.search(title)
 
 a=  Scraper()
-a.get_newest_easy()
+a.get_newest('Intermediate')
 
 
 
